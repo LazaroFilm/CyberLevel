b146573e698f0ab819776fc4b22580e8c85a50df
Victor Lazaro <lazarofilm@gmail.com>
2023-01-22 18:28:37 -0500

    Moved Encoder to it's own tab

--- CyberLevel_Nano_RP_FF/CyberLevel_Encoder.ino
+++ CyberLevel_Nano_RP_FF/CyberLevel_Encoder.ino
@@ -5,7 +5,7 @@
 // decoder state and then (if an event was indicated), the rotaryCount
 // variable is updated.
 // CAUTION: some Arduion functions will not work if called from an ISR.
-void IsrForQDEC(void) { // do absolute minimum possible in any ISR ...
+void IsrForQDEC(void) {  // do absolute minimum possible in any ISR ...
   // placing the `using namespace` line into a function limits
   // allows accessing the types and functions of that namespace,
   // but only while in this function.
@@ -17,4 +17,20 @@ void IsrForQDEC(void) { // do absolute minimum possible in any ISR ...
     rotaryCount = rotaryCount - 1;
   }
   return;
+}
+
+void encoderLoop() {
+  newValue = rotaryCount;
+  if (newValue != lastLoopDisplayedRotaryCount) {
+    // Also get the difference since the last loop() execution
+    int difference = newValue - lastLoopDisplayedRotaryCount;
+
+    // act on the change: e.g., modify PWM to be faster/slower, etc.
+    lastLoopDisplayedRotaryCount = newValue;
+    Serial.print("Change: ");
+    Serial.print(newValue);
+    Serial.print(" (");
+    Serial.print(difference);
+    Serial.println(")");
+  }
 }
